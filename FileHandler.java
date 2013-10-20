451149540da19690ee37a8cb2d09b52226b9cca7
Amin <amin@Amin>
2013-10-20 23:13:43 +0330

    Music and Settings Handlers added, a few fixes on settings activity

---
+++ FileHandler.java
@@ -0,0 +1,17 @@
+package com.example.esmfamil;
+
+public class FileHandler {
+	
+	public static void writeSettingsFile(int sou,int vib, int mus){
+		
+	}	
+	public static int readSettingsFileSound(){
+		return 0;
+	}
+	public static int readSettingsFileMusic(){
+		return 0;
+	}
+	public static int readSettingsFileVibra(){
+		return 0;
+	}
+}
