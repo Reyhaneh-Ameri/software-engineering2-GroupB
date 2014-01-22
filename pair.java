77391e40b2279ff0f91d3fdb6653d15f2c5f2622
qumars <qr7089@gmail.com>
2014-01-22 22:49:52 +0330

    pair for mini game

---
+++ pair.java
@@ -0,0 +1,13 @@
+package com.iust.fandogh;
+
+public class pair {
+	public char c;
+	public int x;
+	public int y;
+	public pair(char c,int x,int y) {
+		this.c=c;
+		this.x=x;
+		this.y=y;
+	}
+
+}
